485f7a06bad3b2e220fcf9c82c4beb6abb17187f
Colin Pear <cpear@hotmail.com>
2016-01-22 12:55:31 -0600

    Added Conference Chairs page. Updated description under why attend.

--- DevOpsConf2016/Controllers/HomeController.cs
+++ DevOpsConf2016/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DevOpsConf2016.Controllers
 {
@@ -23,6 +19,13 @@ namespace DevOpsConf2016.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ConferenceChairs()
+        {
+            ViewBag.Message = "Conference Chairs";
 
             return View();
         }
