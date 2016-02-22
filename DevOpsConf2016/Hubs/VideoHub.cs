6fcea01be346a5e523eb572e964030ddc1d9169f
Javier Lozano <javier@lozanotek.com>
2016-02-22 16:45:24 -0600

    Started adding the video player integration

---
+++ DevOpsConf2016/Hubs/VideoHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace DevOpsConf2016.Hubs
+{
+    public class VideoHub : Hub
+    {
+        //public void Play(string videoId)
+        //{
+        //    Clients.All.PlayVideo(videoId);
+        //}
+    }
+}
